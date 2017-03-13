00c684826eea331df658b8d443412893b66fdd80
olivier <olivier@10.121.61.83>
2017-03-13 10:32:18 +0100

    OMI màj des POJO entities

--- CDI-Rentoweb-EJB/ejbModule/rtw/entity/gererAgence/Adresse.java
+++ CDI-Rentoweb-EJB/ejbModule/rtw/entity/gererAgence/Adresse.java
@@ -10,15 +10,18 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="ADRESSE")
 public class Adresse implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer idAdresse;
 	
 	@Column(name="Voie", length=15, nullable=false)
--- CDI-Rentoweb-EJB/ejbModule/rtw/entity/gererAgence/AgentImmobilier.java
+++ CDI-Rentoweb-EJB/ejbModule/rtw/entity/gererAgence/AgentImmobilier.java
@@ -4,14 +4,17 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="AGENT")
 public class AgentImmobilier implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer idAgentImmobilier;
 	private String nomAgent;
 	private String prenomAgent;
