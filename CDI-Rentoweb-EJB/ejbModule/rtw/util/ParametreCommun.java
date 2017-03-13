0734e93be2a8a7f2ba02611123c2619658d38e90
Aurelien Harle <aurelienharle@hotmail.com>
2017-03-13 13:42:41 +0100

    [CREATE] Aurélien Creation d'une class de parametre commun sous le package util

---
+++ CDI-Rentoweb-EJB/ejbModule/rtw/util/ParametreCommun.java
@@ -0,0 +1,15 @@
+package rtw.util;
+
+/**
+ * Class de paramètres commun au différente fonctionnalité utiliser dans l'application.
+ * 
+ * @author Aurélien
+ * @Version 1
+ * @since 13/03/2017
+ *
+ */
+public class ParametreCommun {
+
+	public static final String PERSISTENCE_UNIT_NAME = "rentoweb";
+	
+}
