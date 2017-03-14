042e779dfb3706abec1ea2ebca6e763a306176f0
Aurelien Harle <aurelienharle@hotmail.com>
2017-03-14 14:42:39 +0100

    [Remove] DaoGererAvis pour compatibilité

--- CDI-Rentoweb-EJB/ejbModule/rtw/dao/gererAvis/daoAvis/daoAvisAnnonce/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Contient toute les classe de la couche DAO Avis Annonce.
- * 
- * @author Aurélien
- * @Version 1
- * @since 13/03/2017
- *
- */
-package rtw.dao.gererAvis.daoAvis.daoAvisAnnonce;
--- CDI-Rentoweb-EJB/ejbModule/rtw/dao/gererAvis/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Contient toute les classe de la DAO de la fonctionnalité gérer avis.
- * 
- * @author Aurélien
- * @version 1
- * @since 06/03/2017
- *
- */
-package rtw.dao.gererAvis;
